046d558fd40ade72ea6029666eff8bcac1a654f4
Ng Cheng Leong <renlore@hotmail.com>
2026-01-06 14:36:50 +0800

    feat: add markdown rendering and preview plugins
    - Add render-markdown.nvim for in-editor markdown rendering
    - Add markdown-preview.nvim for browser preview
    - Add keybindings: <leader>mp (preview), <leader>ms (stop), <leader>mr (toggle)
    - Fix treesitter config for newer API
    - Add markdown and markdown_inline to treesitter parsers

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Markdown
+map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown preview in browser" })
+map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown preview stop" })
+map("n", "<leader>mr", "<cmd>RenderMarkdown toggle<cr>", { desc = "Markdown render toggle" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,19 +13,71 @@ return {
     end,
   },
 
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
   -- Override nvim-treesitter to fix module path issue
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = {
       ensure_installed = {
         "vim", "lua", "vimdoc",
-        "html", "css"
+        "html", "css", "markdown", "markdown_inline",
       },
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+      indent = { enable = true },
     },
     config = function(_, opts)
-      -- Use singular 'config' instead of 'configs'
-      require("nvim-treesitter.config").setup(opts)
+      require("nvim-treesitter").setup(opts)
+    end,
+  },
+
+  -- Render markdown beautifully inside Neovim
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      heading = {
+        enabled = true,
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+      },
+      code = {
+        enabled = true,
+        style = "full",
+        border = "thin",
+      },
+      bullet = {
+        enabled = true,
+        icons = { "●", "○", "◆", "◇" },
+      },
+      checkbox = {
+        enabled = true,
+        unchecked = { icon = "󰄱 " },
+        checked = { icon = "󰱒 " },
+      },
+      quote = { enabled = true },
+      pipe_table = { enabled = true, style = "full" },
+    },
+  },
+
+  -- Live preview in browser (optional - requires Node.js)
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
   },
 }
